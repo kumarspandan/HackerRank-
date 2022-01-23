dcefa19ad6a2cd69f5431d219e28530c17a443b1
Kumar Spandan <72193675+kumarspandan@users.noreply.github.com>
2022-01-23 18:27:18 +0530

    Create Function_Sum of nos.py

---
+++ Function_Sum of nos.py
@@ -0,0 +1,7 @@
+#defining sum function
+def sum(a,b):
+    c=a+b
+    print("The sum of two numbers is: "+str(c))
+
+print("Enter two numbers to add:")
+sum(int(input()), int(input())) #entering the two numbers
